5901294bd649a897579f0787c1bfa81edcfbd028
buelentaksoy <buelent_aksoy@hotmail.de>
2021-05-20 11:11:00 +0200

    Initial commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World-new111244112ccc");
+    System.out.println("Hello, World-jetzt aber zackzack");
   }
 }
